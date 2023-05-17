7e19d6500558193f8130d3fa4a380d17cd79fae8
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2023-05-17 19:00:04 +0300

    Create 02-degiskentanimlama1.js

---
+++ 02-degiskentanimlama1.js
@@ -0,0 +1,16 @@
+//var sözcüğü ile değişken tanımlama
+var myName = "Mustafa";
+
+//değişken tanımlanırken, değeri sonra da atayabiliriz
+var a;
+a = 25;
+
+//bir değişkenin değerini başka bir değişkene de atayabiliriz
+var a;
+a = 45;
+var b;
+b = a;
+
+//metinsel (string) değişken oluştururken tek ya da çift tırnak kullanılabilir
+var myFirstName = "Mustafa";
+var myLastName = 'Buyukdereli';
