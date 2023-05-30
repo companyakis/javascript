07848035be3a972de12017173d5925fbd8a84d49
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2023-05-30 13:35:06 +0300

    Create 04-temelaritmetikislemler.js

---
+++ 04-temelaritmetikislemler.js
@@ -0,0 +1,17 @@
+//+ ile sayıları toplayabiliriz
+
+let toplam = 20 + 12 + 5;
+
+//- ile sayıların farkını elde edebiliriz
+
+let fark = 12 - 5;
+
+//sayıları * ile çarpabiliriz
+
+let carp = 13 * 41;
+
+//sayıları bölerken sağa devrik çizgi / kulllanırız
+
+const bol1 = 22 / 11; //2
+
+const bol2 = 22 / 10; //2.2
