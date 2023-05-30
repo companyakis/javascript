3c2688d64ee1c3c45d2fab195547c2a056420ccf
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2023-05-30 13:45:27 +0300

    Create 06-ondaliklisayilar.js

---
+++ 06-ondaliklisayilar.js
@@ -0,0 +1,11 @@
+//ondalıklı sayılar, decimal numbers veya floats kavramlarını unutmayalım
+
+let ondalikli = 12.12;
+
+//iki ondalıklı sayıyı çarpalım
+
+const carpim = 12.0 * 2.52;
+
+//iki ondalıklı sayıyı bölelim
+
+let bol = 6.4 / 2.0;
