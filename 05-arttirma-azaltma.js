e41c467e6bf86c983f3127aa12c6c54197d7a5a4
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2023-05-30 13:39:37 +0300

    Create 05-arttirma-azaltma.js

---
+++ 05-arttirma-azaltma.js
@@ -0,0 +1,15 @@
+//bir değişkenin değerini 1 arttırmak istesek 2 yol vardır
+
+let a = 5;
+a = a + 1 // a, 6 oldu
+
+let b = 5;
+b++; // b, 6 oldu
+
+//bir değişkenin değerini 1 azaltmak istesek 2 yol vardır
+
+let c = 5;
+c = c - 1; // c, 4 oldu
+
+let d = 5;
+d--; // d, 4 oldu
