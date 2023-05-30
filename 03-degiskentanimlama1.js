5ee8deab66bcd32761b7e7ef0a7b34fbb23ff4b5
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2023-05-30 13:26:13 +0300

    Create 03-degiskentanimlama1.js

---
+++ 03-degiskentanimlama1.js
@@ -0,0 +1,17 @@
+//değişken tanımlarken var yerine, let kullanabiliriz
+
+var ad_1 = "mustafa";
+
+let ad_2 = "Mustafa";
+
+//var ve let arasında bazı farklar olsa bile, genel olarak, let kullanıyorum
+
+//değişken adı birden çok sözcükten oluşuyorsa, JS dilinde camelCase kullanımı yaygındır
+
+let adSoyad = "Mustafa Büyükdereli"; //camelCase
+
+//sabit tanımlarken, const anahtar sözcüğü kullanılır ve büyük harf kullanımı dikkat çeken ve hatayı engelleyebilecek bir uygulamadır
+
+const PI = 3.14;
+
+const KURUCU = "Mustafa Kemal Atatürk";
