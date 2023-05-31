2828e0e05e0642af978f1ad50e5e9f9bc7d58a1c
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2023-05-31 10:19:17 +0300

    Create 08-bilesikatama.js

---
+++ 08-bilesikatama.js
@@ -0,0 +1,28 @@
+//ingilizce olarak compound assignment
+
+//bir değişkenin değerini arttirma -> a = a + b ya da a += b olarak belirtilebilir
+
+let x = 10;
+x += 5; // x = x + 5, x değerini 5 arttırmış oluyoruz, x 15 oldu
+console.log(x); //console.log() ile ekrana yazdırma yapıyoruz, print işlemi
+
+//-----------------------------------------------------------------
+
+//bir değişkenin değerini azaltma -> a = a - b ya da a -= b olarak belirtilebilir
+
+let x = 10;
+x -= 5; // x = x - 5, x değerini 5 azaltmış oluyoruz, x 5 oldu
+
+//-----------------------------------------------------------------
+
+//bir değişkenin değerini çarpma -> a = a * b ya da a *= b olarak belirtilebilir
+
+let x = 10;
+x *= 5; // x = x * 5 , x'i 5 ile çarptık, x 50 oldu
+
+//-----------------------------------------------------------------
+
+//bir değişkenin değerini bölme -> a = a / b ya da a /= b olarak belirtilebilir
+
+let x = 10;
+x /= 5; // x = x / 5 , x'i 5 ile böldük, x 2 oldu
