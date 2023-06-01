76216b2925767f5b92d44c2c326c856983cfbd00
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2023-06-01 14:43:58 +0300

    Create 11-stringbirlestirme.js

---
+++ 11-stringbirlestirme.js
@@ -0,0 +1,31 @@
+//artı + işareti ile string birleştirebiliriz, concatenation operator
+
+ let dil = "Java" + "Script"; //JavaScript
+
+//birleşen ifadeler arasında boşluk olsun istiyorsak, bunu biz sağlayacağız
+
+let s = "Sayı" + " say!"; //Sayı say!
+
+//string birleşimlerinde değişkenleri de kullanabiliriz
+
+let ad = "Mustafa";
+let soyad = " Büyükdereli"; //boşluğa dikkat
+let ben = ad + soyad; //Mustafa Büyükdereli
+
+//+= ile de string birleştirilebilir
+
+let ad = "Mustafa "; //boşluğa dikkat
+ad += "Büyükdereli"; //ad -> Mustafa Büyükdereli
+
+//son 2 örnek
+
+const adim = "Aygün";
+const cumlem = "Benim adim " + adim + " ve 21 yaşındayım.";
+
+//Benim adim Aygün ve 21 yaşındayım.
+
+let birDuygu = "güzel!";
+let cumle = "Özgürce yaşamak ";
+cumle += birDuygu;
+
+Özgürce yaşamak güzel!
