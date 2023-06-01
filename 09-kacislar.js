325603374025ac9bf67d1cd2107f91df6dab27ea
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2023-06-01 14:07:04 +0300

    Create 09-kacislar.js

---
+++ 09-kacislar.js
@@ -0,0 +1,7 @@
+let gunaydin = "Mustafa "Günaydın!" dedi." //sizce hatalı mı?
+
+//yukardaki kodu çalıştırırsak hata alırız. neden mi? tırnakların savaşı:)
+
+const benimGunaydinim = "Mustafa \"Günadyın!\" dedi ve Aygün de \"Anlamadım!\" dedi."; // hata almayacağız! çift tırnaklardan önce sola yatık çizgi eklediğimizi fark ettik mi?
+
+//ingilizce backslash \ deniyor
