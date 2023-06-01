d59f467e9a96fef20578d70f622a64ae08124efc
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2023-06-01 15:02:13 +0300

    Update and rename a.txt to 13-elemanlaraerisme.js

---
+++ 13-elemanlaraerisme.js
@@ -0,0 +1,22 @@
+/*
+let a = "Günaydın";
+bu değişkenin il harfine a[0] ile erişebiliriz
+örneğin üçüncü harfe a[2] ile erişilir
+index numarası 0'dan başlar. ki çoğu yazılım dilinde böyle. lua dilinde 1'den başlar...
+*/
+
+let abc = "ABCÇD";
+
+console.log(abc[0]);
+console.log(abc[1]);
+console.log(abc[2]);
+console.log(abc[3]);
+console.log(abc[4]);
+
+/*
+A
+B
+C
+Ç
+D
+*/
