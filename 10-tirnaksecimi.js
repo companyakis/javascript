67591eeefdb2e89f49a5e535f9cabfeaa86d5826
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2023-06-01 14:15:45 +0300

    Create 10-tirnaksecimi.js

---
+++ 10-tirnaksecimi.js
@@ -0,0 +1,13 @@
+//string'lerde çift tırnak kullanmak zorunda değiliz
+
+//tek tırnak da kullanabiliriz
+
+//ama gerekli durumlarda kaçış \ konusuna dikkat etmemiz gerekir
+
+let ad = "nebahat"; 
+
+let ad_2 = 'aysel'; //tek tırnak kullandık!
+
+let myWeb = '<a href="http://www.akiss.org" >My Web</a>'; //tek ve çift tırnaklar birlikte
+
+const benimIstegim = 'Mustafa, "Hadi, Aysel\'e kahveye gidelim!" dedi.' //Aysel ve e arasında \ kullandığımıza dikkat ettik mi
