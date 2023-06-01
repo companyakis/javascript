601fa154434f83385929491b370fee5a44c10e9b
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2023-06-01 14:52:21 +0300

    Create 12-stringuzunlugu.js

---
+++ 12-stringuzunlugu.js
@@ -0,0 +1,11 @@
+//bir ifadenin uzunluğunu bulalım
+
+//bu hesaplama sırasında boşlukların ve noktalama işaretlerinin de hesaplamaya eklendiğini unutmayalım
+
+console.log("Mustafa".length); //7
+
+console.log("Mustafa ".length); //8, boşluğa dikkat
+
+console.log("Mustafa!".length); //8, ünleme dikkat
+
+console.log("Günaydın hepinize!".length); //18, boşluk ve noktalama işareti
