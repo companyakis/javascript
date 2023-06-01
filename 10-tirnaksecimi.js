fe62f9a2c0384c1cd5dacd7437cfd50a874e2135
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2023-06-01 14:24:24 +0300

    Update 10-tirnaksecimi.js

--- 10-tirnaksecimi.js
+++ 10-tirnaksecimi.js
@@ -11,3 +11,26 @@ let ad_2 = 'aysel'; //tek tırnak kullandık!
 let myWeb = '<a href="http://www.akiss.org" >My Web</a>'; //tek ve çift tırnaklar birlikte
 
 const benimIstegim = 'Mustafa, "Hadi, Aysel\'e kahveye gidelim!" dedi.' //Aysel ve e arasında \ kullandığımıza dikkat ettik mi
+
+//aşağıdaki kullanımlar da gerekli olabilir. her ifadeye Türkçe karşılık aramayacağım bundan sonra; çünkü yazılımın küresel dili ingilizce. gerçek bu:(
+
+//\'	single quote
+//\"	double quote
+//\\	backslash
+//\n	newline
+//\t	tab
+//\r	carriage return
+//\b	backspace
+//\f	form feed
+
+//karmaşık bir örnek yapalım
+
+let hepsi = "ilkSatir\n\t\\ikinciSatir\nucuncuSatir"; 
+
+console.log(hepsi);
+
+/*
+ilkSatir
+	\ikinciSatir
+ucuncuSatir
+*/
