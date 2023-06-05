9d6c2b6133151b8517344d9f1f555bf928c4db15
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2023-06-05 11:55:16 +0300

    Create 16-arrays1.js

---
+++ 16-arrays1.js
@@ -0,0 +1,9 @@
+//çok sayıda veriyi tek bir değişkene atama
+
+const ben = ["mustafa", "türkiye", 29];
+
+console.log(ben); //["mustafa", "türkiye", 29]
+
+//bir array'in öğeleri de array olabilir, nested arrays
+
+let kumeler = [["a", "b"], [1, 2, 3], "günaydın"];
