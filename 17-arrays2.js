21daf4b37f3259eac715b845a7a49eb382353f85
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2023-06-05 12:02:14 +0300

    Create 17-arrays2.js

---
+++ 17-arrays2.js
@@ -0,0 +1,13 @@
+//array öğelerine index ile erişme, index numaraları sıfırdan başlar!
+
+let sayilar = [3, 6, 9, 11];
+
+console.log(sayilar[0]); //index 0, değer 3
+
+console.log(sayilar[2]) //index 2, değer 9
+
+// array elemanlarını değiştirme
+
+sayilar[2] = 444;
+
+console.log(sayilar); //[3, 6, 444, 11]
