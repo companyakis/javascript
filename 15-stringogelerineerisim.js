a55c11e4cbbb41e528016de26db60798169a1533
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2023-06-05 11:06:04 +0300

    Create 15-stringogelerineerisim.js

---
+++ 15-stringogelerineerisim.js
@@ -0,0 +1,11 @@
+let ad = "Mustafa";
+
+let ilk_harf = ad[0];
+
+let ikinci_harf = ad[1];
+
+let son_harf = ad[ad.length - 1]; //ilk öğenin index numarası sıfır olduğundan dolayı
+
+console.log(ilk_harf); //M
+console.log(ikinci_harf); //u
+console.log(son_harf); //a
