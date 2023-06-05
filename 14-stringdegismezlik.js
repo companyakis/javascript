ab185723f576bd9c725d2d12a97ec0cf92682fab
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2023-06-05 10:57:10 +0300

    Create 14-stringdegismezlik.js

---
+++ 14-stringdegismezlik.js
@@ -0,0 +1,11 @@
+//string immutability konusu
+
+let ben = "Mustafa";
+
+ben[0] = "N"; //hatalı kullanım
+
+//eğer adı Nustafa yapmak istiyorsak, atama yapmalıyız
+
+ben = "Nustafa"; 
+
+console.log(ben); //Nustafa
