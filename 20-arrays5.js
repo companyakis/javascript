88a86981ea1c87aa82cdd9475bbe6c6dfff76f67
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2023-06-05 12:34:31 +0300

    Update 20-arrays5.js

--- 20-arrays5.js
+++ 20-arrays5.js
@@ -1,3 +1,5 @@
+//pop ve shift işlevleri!
+
 //pop() işlevi ile sondan öğe silme
 
 let sayilar = [0, 1, 2, 3, 4, 5];
@@ -13,3 +15,19 @@ const silinenIki = sayilar.pop();
 console.log(silinenIki); //4
 
 console.log(sayilar[silinenIki]); //?
+
+//----------------------------------------
+
+//shift() işlevi ilk öğeyi silme
+
+let sayilar = [0, 1, 2, 3, 4, 5];
+
+sayilar.shift();
+
+console.log(sayilar); //[1, 2, 3, 4, 5]
+
+//silinen değeri değişkene atama
+
+let a = sayilar.shift();
+
+console.log(a); //1
