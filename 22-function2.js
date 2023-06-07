e69af6c88f7472cf62d22be04fc1b11974af6750
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2023-06-07 20:33:36 +0300

    Create 22-function2.js

---
+++ 22-function2.js
@@ -0,0 +1,13 @@
+//kullanıcı tanımlı bir işleve parameters verilmesi
+
+function functionWithParameters(sayi1, sayi2) {
+  console.log(sayi1 + sayi2);
+}
+
+functionWithParameters(3, 5); //8
+
+functionWithParameters(3, 5.4); //8.4
+
+functionWithParameters("günaydın", 5.4); //günaydın5.4
+
+//metin ile sayının toplanabilmesi JavaScript'i ilginç yapıyor. TypeScript diyenler haklı mı yoksa:)
