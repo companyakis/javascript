dadcaf643089b65c48e3dc830a5316456d774722
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2023-06-07 20:25:52 +0300

    Create 21-function1.js

---
+++ 21-function1.js
@@ -0,0 +1,11 @@
+//function tanımlama ve çağırma
+
+function firstFunction() {
+  console.log("Günaydın!");
+} 
+
+//dilediğimiz kadar, tanımladığımız bir işlevi çağırabiliriz
+
+firstFunction();
+firstFunction();
+firstFunction();
