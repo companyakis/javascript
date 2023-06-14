d6640d143168583ea301105583c5c1340fe5af50
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2023-06-14 10:56:11 +0300

    Create 26-function-degiskene-atama.js

---
+++ 26-function-degiskene-atama.js
@@ -0,0 +1,13 @@
+//bir işlevden dönen değeri bir değişkene atayabiliriz
+
+function sayiIslemleri(sayi) {
+  return (sayi + 23) / 4;
+}
+
+let sonuc1 = sayiIslemleri(1)
+
+console.log(sonuc1); //6
+
+let sonuc2 = sayiIslemleri(21)
+
+console.log(sonuc2); //11
